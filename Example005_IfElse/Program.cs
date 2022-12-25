4ada4c2a02cf1b89216994014a97dc804224f346
Endnes <sergeev.krut@gmail.com>
2022-12-25 13:45:48 +0300

    Добавлен еще пример

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username == "маша"){
+    Console.WriteLine("Ура, это МАША");
+}
+else
+{
+    Console.
+}
